cd817f494a85130f142c58a1300c68a16e442630
olaluwoye9 <olaluwoye9@gmail.com>
2025-03-18 11:12:34 +0000

    First commit

---
+++ calculator.py
@@ -0,0 +1,15 @@
+"""
+*calculator.py it contains a calculator implementation.
+"""
+
+def summation(a:int,b:int)->int:
+    """
+    Summary:
+
+    Input:
+
+    Output:
+    """
+    return a+b
+def multiplication(a:int,b:int)->int:
+    return a*b
---
+++ main.py
@@ -0,0 +1,8 @@
+from calculator import summation,multiplication
+
+if __name__=="__main__":
+    a = 4
+    b = 3
+    print(f"Summation of {a}+{b}={summation(a,b)}")
+    print(f"Summation of {a}x{b}={multiplication(a,b)}")
+    print("Hello world")
